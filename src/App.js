47e317ede59d267c2e2d8d422ffb2688a2b8a4b5
higa111-source <tttt_higa111@yahoo.co.jp>
2025-08-23 17:15:56 +0900

    初回コミット

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@
 import { useState } from 'react'
 import ExpenseList from './components/ExpenseList'
 import ExpenseForm from './components/ExpenseForm'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   const [reload, setReload] = useState(false)
 
   return (
-    <div>
-      <h1>家計簿アプリ</h1>
-      <ExpenseForm onAdd={() => setReload(!reload)} />
-      <ExpenseList key={reload} />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">支出追加</Link> |{" "}
+          <Link to="/list">支出一覧</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<ExpenseForm />} />
+          <Route path="/list" element={<ExpenseList />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
